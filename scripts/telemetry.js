305f809343a0352781cd7946b60eb0cde08d4937
aagboola-ghub <aagboola@marchex.com>
2025-08-10 18:50:42 -0400

    Delete scripts directory

--- scripts/telemetry.js
+++
@@ -1,5 +0,0 @@
-
-const bus = new EventTarget();
-export function on(evt, handler){ bus.addEventListener(evt, handler); }
-export function off(evt, handler){ bus.removeEventListener(evt, handler); }
-export function emit(name, detail={}){ bus.dispatchEvent(new CustomEvent(name, { detail: Object.assign({ ts: Date.now() }, detail) })); }
