2132a91cfaf8fe319cbdae6cc952e8a99517049e
aagboola-ghub <aagboola@marchex.com>
2025-08-11 07:03:02 -0400

    Delete scripts directory

--- scripts/config/loader.js
+++
@@ -1,8 +0,0 @@
-
-export async function loadConfig(){
-  const params = new URLSearchParams(location.search);
-  const id = params.get('config') || 'wells-fargo';
-  const res = await fetch(`./configs/${id}.json`);
-  if(!res.ok) throw new Error(`Config not found: ${id}`);
-  return await res.json();
-}
